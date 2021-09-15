ad9b73308e2c185dbd889f8d8e7f4a3f5c9f8612
Mr.XY <10951630+XYCaptain@users.noreply.github.com>
2021-09-15 12:51:48 +0800

    Fix Des

---
+++ tests/SharpGLTF.Toolkit.Tests/Extensions/TestExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.Parametric;
+using SharpGLTF.Geometry.VertexTypes;
+using SharpGLTF.Materials;
+using SharpGLTF.Scenes;
+using SharpGLTF.Schema2;
+using SharpGLTF.Transforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace SharpGLTF.Extensions
+{
+    internal class TestExtensions
+    {
+        [Test]
+        public void TestGltf()
+        {
+            var scene = new SharpGLTF.Scenes.SceneBuilder();
+
+            var material = new MaterialBuilder("material1")
+                .WithDoubleSide(false)
+                .WithMetallicRoughnessShader()
+                .WithBaseColor(new Vector4(0.5f, 0.3f, 0.4f, 0.8f));
+
+            var material1 = new MaterialBuilder("material1")
+                .WithDoubleSide(false)
+                .WithMetallicRoughnessShader()
+                .WithBaseColor(new Vector4(0.6f, 0.8f, 0.7f, 1f));
+
+            var mesh = new MeshBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
+            mesh.AddCube(material, Matrix4x4.Identity);
+            mesh.AddCube(material1, Matrix4x4.CreateTranslation(0.5f, 0.5f, 0.5f));
+
+            var root = new NodeBuilder("root");
+            scene.AddRigidMesh(mesh, root);
+            var node1 = new NodeBuilder("instance");
+            var root1 = new NodeBuilder("root1");
+            scene.AddRigidMesh(mesh, root1, AffineTransform.CreateFromAny(null, new Vector3(5, 5, 5), null, new Vector3(20, 20, 20)));
+            var model = scene.ToGltf2();
+
+            List<AffineTransform> ass = new List<AffineTransform>();
+            var rand = new Random();
+            List<object> fids = new List<object>();
+            List<building> bs = new List<building>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                fids.Add(i);
+                bs.Add(new building() { height = i, id = i });
+                ass.Add(AffineTransform.CreateFromAny(null, new Vector3(1 * (float)rand.NextDouble() + 0.5f, 1 * (float)rand.NextDouble() + 0.5f, 1 * (float)rand.NextDouble() + 0.5f), null, new Vector3(10 * (float)rand.NextDouble(), 10 * (float)rand.NextDouble(), 10 * (float)rand.NextDouble())));
+            }
+
+            model.LogicalNodes.First().UseGpuInstancing().WithInstanceAccessors(ass).WithInstanceCustomAccessor("_FEATURE_ID_0", fids);
+            model.LogicalNodes.First().UseFeatureMetadata().SetFeatureData("building", "_FEATURE_ID_0");
+
+            var f = model.UseFeatureMetadata();
+            f.WithFeatureAccessors(bs);
+            var text = "{\"classes\":{\"building\":{\"name\":\"building\",\"properties\":{\"height\":{\"name\":\"height\",\"type\":\"INT8\",\"componentType\":0,\"componentCount\":0,\"normalized\":false,\"max\":0.0,\"min\":0.0,\"default\":0.0,\"optional\":false}}}}}";
+            f.SetShcema(text);
+            model.SetExtension(f);
+
+            model.SaveGLTF(@$"C:\Users\Liuxinyu\Documents\cesium\Specs\Data\Models\GltfLoader\BuildingsMetadata\glTF\preview.gltf", new WriteSettings() { MergeBuffers = false });
+            model.SaveGLB(@$"C:\Users\Liuxinyu\Documents\cesium\Specs\Data\Models\GltfLoader\BuildingsMetadata\glTF\preview.glb", new WriteSettings() { MergeBuffers = false });
+        }
+    }
+
+    public class building
+    {
+        [JsonProperty("height")]
+        public float height { get; set; }
+        [JsonProperty("id")]
+        public int id { get; set; }
+    }
+}
--- tests/SharpGLTF.Toolkit.Tests/Scenes/SceneBuilderTests.cs
+++ tests/SharpGLTF.Toolkit.Tests/Scenes/SceneBuilderTests.cs
@@ -901,70 +901,8 @@ namespace SharpGLTF.Scenes
             return mesh1;
         }
 
-        [Test]
-        public void GltfTest()
-        {
-            var scene = new SharpGLTF.Scenes.SceneBuilder();
-
-            var material = new MaterialBuilder("material1")
-                .WithDoubleSide(false)
-                .WithMetallicRoughnessShader()
-                .WithBaseColor(new Vector4(0.5f, 0.3f, 0.4f, 0.8f));
-
-            var material1 = new MaterialBuilder("material1")
-                .WithDoubleSide(false)
-                .WithMetallicRoughnessShader()
-                .WithBaseColor(new Vector4(0.6f, 0.8f, 0.7f, 1f));
-
-            var mesh = new MeshBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
-            mesh.AddCube(material, Matrix4x4.Identity);
-            mesh.AddCube(material1, Matrix4x4.CreateTranslation(0.5f, 0.5f, 0.5f));
-
-            var root = new NodeBuilder("root");
-            scene.AddRigidMesh(mesh, root);
-            var node1 = new NodeBuilder("instance");
-            var root1 = new NodeBuilder("root1");
-            scene.AddRigidMesh(mesh, root1, AffineTransform.CreateFromAny(null, new Vector3(5, 5, 5), null, new Vector3(20, 20, 20)));
-            var model = scene.ToGltf2();
-            List<AffineTransform> ass = new List<AffineTransform>();
-            var rand = new Random();
-            List<object> fids = new List<object>();
-            List<building> bs = new List<building>();
-            List<building2> bs1 = new List<building2>();
-
-            for (int i = 0; i < 100; i++)
-            {
-                fids.Add(i);
-                bs.Add(new building() { height = i, id = i });
-                bs1.Add(new building2() { height = i, id = i });
-                ass.Add(AffineTransform.CreateFromAny(null, new Vector3(1 * (float)rand.NextDouble() + 0.5f, 1 * (float)rand.NextDouble() + 0.5f, 1 * (float)rand.NextDouble() + 0.5f), null, new Vector3(10 * (float)rand.NextDouble(), 10 * (float)rand.NextDouble(), 10 * (float)rand.NextDouble())));
-            }
-            model.LogicalNodes.First()/*.UseGpuInstancing().WithInstanceAccessors(ass).WithInstanceCustomAccessor("_FEATURE_ID_0", fids)*/;
-            //model.LogicalNodes.First().UseFeatureMetadata().SetFeatureData("building", "_FEATURE_ID_0");
-            //var f = model.UseFeatureMetadata();
-            //f.WithFeatureAccessors(bs);
-            //f.WithFeatureAccessors(bs1);
-            //f.SetShcema("{\"123\":213}");
-            //model.SetExtension(f);
-
-            model.SaveGLTF(@$"preview.gltf", new WriteSettings() { MergeBuffers = false });
-            model.SaveGLB(@$"C:\Users\Liuxinyu\Documents\cesium\Specs\Data\Models\GltfLoader\BuildingsMetadata\glTF\preview.glb", new WriteSettings() { MergeBuffers = false });
-        }
+   
     }
 
-    public class building
-    {
-        [JsonProperty("height")]
-        public float height { get; set; }
-        [JsonProperty("id")]
-        public int id { get; set; }
-    }
 
-    public class building2
-    {
-        [JsonProperty("height")]
-        public float height { get; set; }
-        [JsonProperty("id")]
-        public int id { get; set; }
-    }
 }
